0549b9a970fb289c1b2e6031ccc3ad555dc2eb07
Terry Zhang <tz624@nyu.edu>
2020-05-29 10:06:31 +0800

    update router folder structure

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,13 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
+import HomeRoute from './home';
+import AboutRoute from './about';
 
 Vue.use(VueRouter);
 
 export const routes: Array<RouteConfig> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue'),
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
+  HomeRoute,
+  AboutRoute,
 ];
 
 const router = new VueRouter({
