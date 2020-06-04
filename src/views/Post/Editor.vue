c5a8b3580796b90157a247a4e598ee3d6ccc7891
Terry Zhang <tz624@nyu.edu>
2020-06-04 22:21:28 +0800

    Editor using js

---
+++ src/views/Post/Editor.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <editor-content :editor="editor" />
+  </div>
+</template>
+
+<script>
+import { Component, Vue } from 'vue-property-decorator';
+import { Editor, EditorContent } from 'tiptap';
+
+@Component({
+  name: 'about',
+  components: {
+    EditorContent,
+    Editor,
+  },
+})
+export default class extends Vue {
+  editor = new Editor({
+    content: '<p>Initial editor content</p>',
+  });
+
+  beforeDestroy() {
+    this.editor.destroy();
+  }
+}
+</script>
