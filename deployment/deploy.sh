c050273ff7a191746cbcea0647fbeb700e0afbae
Terry Zhang <tz624@nyu.edu>
2020-06-23 15:47:25 +0800

    fix some config

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -4,6 +4,5 @@
 # 1. Docker installed on remote machine
 # 2. Docker Compose installed on remote machine
 
-docker-compose -f nginx-proxy-compose.yaml up -d --build || exit
 docker-compose -f web-app-compose.yaml build --build-arg app_env=production || exit
 docker-compose -f web-app-compose.yaml up -d || exit
